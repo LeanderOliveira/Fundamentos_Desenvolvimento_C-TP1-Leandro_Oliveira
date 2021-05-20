fe9bd137fe9b5fe6d562cdc7e4e9213c06b12961
LeanderOliveira <74795055+LeanderOliveira@users.noreply.github.com>
2021-05-20 11:41:10 -0300

    Add files via upload

---
+++ Adicao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Adicao
+{
+    class Adicao
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Esse projeto realiza soma.");
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro nº: ");
+            while (!int.TryParse(Console.ReadLine(), out a)) //Verifica se o que foi digitado é número
+            {
+                Console.WriteLine("Erro! Insira apenas números inteiros.");
+            }
+
+            Console.Write("Digite o segundo nº: ");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Erro! Insira apenas números inteiros.");
+            }
+
+            int resultado = a + b;
+            Console.WriteLine($"O resultado da soma de {a} e {b} é: {resultado}");
+        }
+    }
+}
---
+++ Divisao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Divisao
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Esse projeto realiza divisão.");
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro nº: ");
+            while (!int.TryParse(Console.ReadLine(), out a)) //Verifica se o que foi digitado é número
+            {
+                Console.WriteLine("Erro! Insira apenas números inteiros.");
+            }
+
+            Console.Write("Digite o segundo nº: ");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Erro! Insira apenas números inteiros.");
+            }
+
+            int resultado = a / b;
+            Console.WriteLine($"O resultado da divisão de {a} e {b} é: {resultado}");
+        }
+    }
+}
